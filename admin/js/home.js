f2c67366de5fecc1df59e965ff3799b609a8f2bb
uuxia <uuxia@uuxia.cn>
2022-09-26 16:01:23 +0800

    2022-09-26 16:01:22 by root

--- admin/js/home.js
+++ admin/js/home.js
@@ -23,10 +23,34 @@ function gotoLogs(){
     window.location.href = '../log/index.html'
 }
 
+function gotoDnsPod(){
+    window.location.href = '../dns/index.html'
+}
+
 function gotoDevices(){
     window.location.href = '../device/index.html'
 }
 
 function gotoTasks(){
     window.location.href = '../task/index.html'
+}
+
+function createRecord(){
+    var value = { "domain": document.getElementById('domain_id').value,
+        "subDomain": document.getElementById('subdomain_id').value,
+        "recordType": document.getElementById('recordtype_id').value,
+        "recordLine": document.getElementById('recordLine_id').value,
+        "value": document.getElementById('value_id').value
+    };
+    jQuery.ajax({
+        //提交的网址
+        type: 'POST',
+        url: host + "/v1/api/dnspod/createRecord",
+        data: value,
+        contentType: "application/x-www-form-urlencoded",
+        success: function(results) {
+            console.log("####login " + JSON.stringify(results));
+            toast(JSON.stringify(results));
+        }
+    });
 }
